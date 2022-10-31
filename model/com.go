b51883c98c8520b8e66896e3ddbb82eb94e1d62d
Luminescent0 <2460149117@qq.com>
2022-11-01 00:39:25 +0800

    func:新增定时器功能

--- model/com.go
+++ model/com.go
@@ -8,19 +8,20 @@ package model
 // 事件相关结构体，详情见https://docs.go-cqhttp.org/event
 
 type Com struct {
-	Time     int64  `json:"time,omitempty"`
-	SelfId   int64  `json:"self_id,omitempty"`
-	PostType string `json:"post_type,omitempty"`
+	Time     int64  `json:"time,omitempty"`      //事件发生的时间戳
+	SelfId   int64  `json:"self_id,omitempty"`   //收到事件的机器人的qq
+	PostType string `json:"post_type,omitempty"` //上报类型
 }
 
+// Message post_type为message的上报
 type Message struct {
 	Com
 	MessageType string `json:"message_type"`
-	SubType     string `json:"sub_type"`
+	SubType     string `json:"sub_type"` //消息的子类型 可能为group,public
 	MessageId   int32  `json:"message_id"`
-	UserId      int64  `json:"user_id"`
+	UserId      int64  `json:"user_id"` //发送者的qq
 	Messages    string `json:"message"`
-	RawMessage  string `json:"raw_message"`
+	RawMessage  string `json:"raw_message"` //CQ code格式的消息
 	NoticeType  string `json:"notice_type"`
 	Font        string `json:"font"`
 	GroupId     int64  `json:"group_id"`
---
+++ service/chenxian.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"fwbot/model"
+	"github.com/robfig/cron/v3"
+	//"math/rand"
+	"strings"
+	//"time"
+)
+
+const (
+	XianUserId = "2945008714"
+	XianCorn   = "定时任务"
+)
+
+func XianDefaultFunc() error {
+	//rand.Seed(time.Now().Unix())
+	WsPrivateMsg("听不懂捏", XianUserId)
+	return nil
+}
+func XianSetCorn(msg model.Message) error {
+	if !strings.HasPrefix(msg.Messages, XianCorn) {
+		return XianDefaultFunc()
+	}
+	res := strings.Split(msg.Messages, " ")
+	task := res[1]
+	spec := res[2]
+	resp := task + "完成了吗"
+	c := cron.New(cron.WithSeconds()) //withSeconds精确到秒
+	_, err := c.AddFunc(spec, func() {
+		WsPrivateMsg(resp, XianUserId)
+	})
+	if err != nil {
+		return err
+	}
+	c.Start()
+	defer c.Stop()
+	return nil
+}
